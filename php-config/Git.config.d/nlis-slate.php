9f5633db8f555dbf08ee83c7dd66191060efbe82
Chris Alfano <chris@jarv.us>
2022-04-07 15:09:39 -0400

    feat(ci): configure VFS<->Git mapping

---
+++ php-config/Git.config.d/nlis-slate.php
@@ -0,0 +1,10 @@
+<?php
+
+Git::$repositories['nlis-slate'] = [
+    'remote' => 'git@github.com:b21network/nlis-slate.git',
+    'originBranch' => 'emergence/layer/v1',
+    'workingBranch' => 'emergence/layer/v1',
+    'trees' => [
+        'php-config/Git.config.d/nlis-slate.php',
+    ]
+];
