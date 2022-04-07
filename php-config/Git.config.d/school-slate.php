351b6b79c285e31cc6e3ecc7d6a2ccb61544067c
Chris Alfano <chris@jarv.us>
2022-04-07 15:22:36 -0400

    feat(cbl): initialize frontend package

---
+++ php-config/Git.config.d/school-slate.php
@@ -0,0 +1,10 @@
+<?php
+
+Git::$repositories['school-slate'] = [
+    'remote' => 'git@github.com:b21network/school-slate.git',
+    'originBranch' => 'emergence/layer/v1',
+    'workingBranch' => 'emergence/layer/v1',
+    'trees' => [
+        'php-config/Git.config.d/school-slate.php',
+    ]
+];
---
+++ sencha-workspace/packages/school-cbl-config/overrides/Config.js
@@ -0,0 +1,22 @@
+Ext.define(null, {
+    override: 'Slate.cbl.util.Config'
+}, function(Config) {
+    Config.setLevels({
+        9: {
+            title: 'Portfolio 1',
+            abbreviation: 'P1'
+        },
+        10: {
+            title: 'Portfolio 2',
+            abbreviation: 'P2'
+        },
+        11: {
+            title: 'Portfolio 3',
+            abbreviation: 'P3'
+        },
+        12: {
+            title: 'Portfolio 4',
+            abbreviation: 'P4'
+        }
+    });
+});
