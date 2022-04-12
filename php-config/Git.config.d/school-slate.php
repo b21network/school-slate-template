7086f3ccfbc1a50c6fd239244acccb284548548b
Chris Alfano <chris@jarv.us>
2022-04-11 23:38:35 -0400

    fix(ci): add all paths to VFS git mapping

--- php-config/Git.config.d/school-slate.php
+++ php-config/Git.config.d/school-slate.php
@@ -5,6 +5,12 @@ Git::$repositories['school-slate'] = [
     'originBranch' => 'emergence/layer/v1',
     'workingBranch' => 'emergence/layer/v1',
     'trees' => [
+        'php-config/Emergence/WebApps/SenchaApp.config.d/school-cbl-config.php',
         'php-config/Git.config.d/school-slate.php',
+        'php-config/Slate/CBL/Demonstrations/ExperienceDemonstration.config.d/experience-type.php',
+        'php-config/Slate/CBL/Demonstrations/ExperienceDemonstration.config.d/performance-type.php',
+        'php-config/Slate/CBL/StudentCompetency.config.d/minimum-average.php',
+        'php-config/Slate/CBL/Tasks/ExperienceTask.config.d/experience-type.php',
+        'webapp-plugin-builds/school-cbl-config'
     ]
 ];
