53e7bb0e110596b4c33c834a39bffdd728299bec
Chris Alfano <chris@jarv.us>
2022-08-02 12:11:22 -0400

    refactor: simplify git mappings for layer

--- php-config/Git.config.d/school-slate.php
+++ php-config/Git.config.d/school-slate.php
@@ -5,12 +5,9 @@ Git::$repositories['school-slate'] = [
     'originBranch' => 'emergence/layer/v1',
     'workingBranch' => 'emergence/layer/v1',
     'trees' => [
-        'php-config/Emergence/WebApps/SenchaApp.config.d/school-cbl-config.php',
-        'php-config/Git.config.d/school-slate.php',
-        'php-config/Slate/CBL/Demonstrations/ExperienceDemonstration.config.d/experience-type.php',
-        'php-config/Slate/CBL/Demonstrations/ExperienceDemonstration.config.d/performance-type.php',
-        'php-config/Slate/CBL/StudentCompetency.config.d/minimum-average.php',
-        'php-config/Slate/CBL/Tasks/ExperienceTask.config.d/experience-type.php',
-        'webapp-plugin-builds/school-cbl-config'
+        // 'content-blocks',
+        'php-config',
+        'webapp-plugin-builds',
+        // 'site-root'
     ]
 ];
